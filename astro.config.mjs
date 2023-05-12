d2bb0ad28c9ddab38e2c44350192dd06b22553e4
Peter Garside-Quin <petegq@gmail.com>
2023-05-12 14:50:27 +0100

    Change theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+  site: 'https://wtzsh.netlify.app',
+  integrations: [mdx(), svelte()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord',
+    },
+    remarkPlugins: ['remark-gfm', 'remark-smartypants'],
+    rehypePlugins: [
+      [
+        'rehype-external-links',
+        {
+          target: '_blank',
+        },
+      ],
+    ],
+  },
+})
